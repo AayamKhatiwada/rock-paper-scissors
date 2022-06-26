feb5239592065dc953ff29d000df1e31b2c9e497
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-26 07:44:03 +0545

    Update App.js

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className='name'>Rock Paper Scissors</div>
+      <div className='Player'>
+        <h1 className='playerName'>Player 1</h1>
+        <img src=''></img>
+      </div>
     </div>
   );
 }
