3ae9e9d6374fac71825121e9e89d9ac82f80ef12
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-06-26 13:55:59 +0545

    Update App.js

--- rock_paper_scissors/src/App.js
+++ rock_paper_scissors/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { useState , useEffect } from 'react';
 
 function App() {
+  const [state, changeState] = useState('rock')
+  const [comState, comChangeState] = useState('rock')
+  const [userPoint, userChangePoint] = useState(0)
+  const [comPoint, comChangePoint] = useState(0)
+
   return (
     <div className="App">
-      <div className='name'>Rock Paper Scissors</div>
-      <div className='Player'>
-        <h1 className='playerName'>Player 1</h1>
-        <img src=''></img>
+      <h1>Rock Paper Scissors</h1>
+      <div className='board'>
+        <div className='user'>
+          <span>User Score : {userPoint}</span>
+        </div>
+        <div className='computer'>
+          <span>Computer Score : {comPoint}</span>
+        </div>
       </div>
     </div>
   );
